f637048ba697dad2b6db5d7c788f50bca34465ad
Harish Hulyalkar <hulyalkarharish@gmail.com>
2026-08-23 23:00:33 +0530

    Variable created

--- LiftApp.java
+++ LiftApp.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class LiftApp{
 public static void main(String [] args){
 Scanner sc = new Scanner(System.in);
+String liftName="*****UparNiche*****";
 }
 
 public static void liftOne(){
